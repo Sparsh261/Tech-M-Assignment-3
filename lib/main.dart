4277d7a5d93b62cfd486711d6475142560673783
Sparsh261 <sparshchauhan112@gmail.com>
2025-06-10 15:14:10 +0530

    Added Workout items and BMI page

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    // home: FirstPage()
-    home: Home(),
+    home: FirstPage(),
     debugShowCheckedModeBanner: false,
   ));
 }
